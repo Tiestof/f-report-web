145b8279fe7fe9f3fb4620e3a90344d54d4fa927
Franco Gatica <fg250107@ncr.com>
2025-04-27 19:37:22 -0400

    config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,5 @@
 const config = {
-    apiUrl: 'https://194.195.87.132:3000/api',
-    //  usar local en desarrollo:
-    // apiUrl: 'https://localhost:3000/api',
-};
-
-export default config;
+    apiUrl: 'https://f-report.xyz:3000/api'
+  };
+  
+  export default config;
