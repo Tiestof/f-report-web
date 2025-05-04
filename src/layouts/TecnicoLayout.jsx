cf9b21dfef3b0f7ce1f26982e205c8a61fa86546
Franco Gatica <fg250107@ncr.com>
2025-05-03 23:12:43 -0400

    Mejora menor en layout de Supervisor y Tecnico  Ajuste visual

--- src/layouts/TecnicoLayout.jsx
+++ src/layouts/TecnicoLayout.jsx
@@ -5,7 +5,7 @@ export default function TecnicoLayout() {
   return (
     <div className="flex">
       <Menu perfil="TÉCNICO" />
-      <div className="flex-1 p-6 bg-gray-100 min-h-screen">
+      <div className="flex-1 p-6 bg-gray-100 min-h-screen pt-[72px] md:pt-6">
         <Outlet />
       </div>
     </div>
